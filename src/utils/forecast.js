b8e9c841bad3742a2dbf80cb6474a1802278ed6d
Gautam Saini <gautamsaini455@gmail.com>
2020-03-17 11:45:34 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/73a0a30b953e7f65cb8a103d8ada6e27/'+ latitude + ',' + longitude + '?units=si'
+    
+    
+    request({ url, json: true}, (error, {body} ) => {
+        if(error){
+            callback('Unable to connect to Weather App', undefined)
+        } else if (body.error){
+            callback('Unable to find location', undefined)
+        } else{
+            callback(undefined, body.daily.data[0].summary +  'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+        }
+    })
+}
+
+module.exports = forecast
